dac18ea4b1f3dc6858413798464d957e0e5e0806
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-22 22:30:22 +0000

    Update global usings file

--- AssetHelper/GlobalUsings.cs
+++ AssetHelper/GlobalUsings.cs
@@ -1,2 +1,5 @@
-﻿global using UObject = UnityEngine.Object;
+﻿// As a style preference, this file should only contain aliases (global using X = Y)
+
+global using UObject = UnityEngine.Object;
 global using Logger = BepInEx.Logging.Logger;
+global using USceneManager = UnityEngine.SceneManagement.SceneManager;
