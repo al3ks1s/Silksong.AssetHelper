f0175f0d38ff70bb17654305b7cc55e0f9382fed
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-25 12:19:35 +0000

    Don't recalculate on patch version delta

--- AssetHelper/Internal/CacheManager.cs
+++ AssetHelper/Internal/CacheManager.cs
@@ -8,8 +8,9 @@ internal static class CacheManager
 {
     private static readonly ManualLogSource Log = Logger.CreateLogSource($"{nameof(AssetHelper)}.{nameof(CacheManager)}");
 
-    // TODO - only recalculate if major/minor version changes
-
+    /// <summary>
+    /// Write an object to the cache, regardless of whether or not is already there.
+    /// </summary>
     public static void WriteObj<T>(T obj, string filename) where T : class
     {
         string filePath = Path.Combine(AssetPaths.CacheDirectory, filename);
@@ -18,12 +19,27 @@ internal static class CacheManager
         toCache?.SerializeToFile(filePath);
     }
 
+    private static bool VersionMatches(string? self, string? other)
+    {
+        if (self is null || other is null)
+        {
+            return false;
+        }
+
+        if (!Version.TryParse(self, out Version selfV) || !Version.TryParse(other, out Version otherV))
+        {
+            return false;
+        }
+
+        return selfV.Major == otherV.Major && selfV.Minor == otherV.Minor;
+    }
+
     /// <summary>
     /// If the filename exists in the cache folder, load the object using Newtonsoft.Json.
     /// If the filename does not exist, compute the object, store it in the file and return it.
     /// 
     /// The function will be recalculated each time the Silksong version or
-    /// <see cref="AssetHelperPlugin"/> version changes.
+    /// <see cref="AssetHelperPlugin"/> major/minor version changes.
     /// </summary>
     /// <typeparam name="T">The type of the object.</typeparam>
     /// <param name="generator">The function used to generate the object.</param>
@@ -37,7 +53,7 @@ internal static class CacheManager
 
         if (JsonExtensions.TryLoadFromFile<VersionedObject<T>>(filePath, out VersionedObject<T>? fromCache))
         {
-            if (fromCache.Version == AssetHelperPlugin.Version && fromCache.Value is not null)
+            if (fromCache.Value is not null && VersionMatches(fromCache.Version, AssetHelperPlugin.Version))
             {
                 return fromCache.Value;
             }
