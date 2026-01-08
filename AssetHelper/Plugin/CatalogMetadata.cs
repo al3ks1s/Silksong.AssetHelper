41cf1c53e6981649aea9456e0a87de150525a13f
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-01-08 06:07:05 +0000

    Add doc

--- AssetHelper/Plugin/CatalogMetadata.cs
+++ AssetHelper/Plugin/CatalogMetadata.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Silksong.AssetHelper.Plugin;
 
-internal abstract class CatalogMetadata
+internal class CatalogMetadata
 {
     public string SilksongVersion { get; set; } = AssetPaths.SilksongVersion;
 
@@ -20,6 +20,9 @@ internal class SceneCatalogMetadata : CatalogMetadata
     // TODO - list catalogued objects
 }
 
+/// <summary>
+/// Data about the non-scene asset catalog written by AssetHelper.
+/// </summary>
 internal class NonSceneCatalogMetadata : CatalogMetadata
 {
     [JsonConverter(typeof(DictListConverter<(string, string), Type>))]
